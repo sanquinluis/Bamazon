9a15c288d66a3e9e2b4694bc911e8c14e12dcc59
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-09-19 01:06:35 -0400

    Working on the second function

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -11,61 +11,76 @@ var connection = mysql.createConnection({
 });
  
 connection.connect();
-
-	var schema = {
-    		properties: {
-     	 		product_Id: {
-        			message: colors.blue('Select your product by Id'),
-        			required: true
+		var schema = {
+    			properties: {
+     	 			product_Id: {
+        				message: colors.blue('Select your product by Id'),
+        				required: true
       },
-     			quantity: {
-     				message: colors.blue('How many units of the product would you like to buy?'),
-        			required: true
+     				quantity: {
+     					message: colors.blue('How many units of the product would you like to buy?'),
+        				required: true
       }
 
     }
 
   };
+
+	
 	showMeProduct();
+//------------------------------------------------------------------------------------
+	//First function that prints the products. 
 	function showMeProduct() {
  	connection.query('SELECT * FROM Bamazon.Products ', function(err, result) {
  			 	if (err) throw err;
  			
- 			console.log(colors.red("Welcome to Bamazon!!")); 
+ 				console.log(colors.red("Welcome to Bamazon!!")); 
 
  			for (var i = 0; i < result.length; i++){
-			console.log(result[i].idProducts + " " + "'" + 
+				console.log(result[i].idProducts + " " + "'" + 
 						result[i].ProductName + "'" + " " + 
 						result[i].DepartmentName + colors.green(" $") + 
 						result[i].Price + " " + 
 						result[i].StockQuantity + colors.magenta(" Units"));	
 		}
-});
+})
 
   	prompt.start();
 
  	prompt.get(schema, function (err, result) {
  				if (err) throw err;
  				// console.log('Command-line input received:');
- 				console.log(product_Id.result);
- 				console.log(quantity.result);
 
- 				selection = result.idProducts;
- 				amount = result.StockQuantity;
+ 				selection = result.product_Id;
+ 				amount = result.quantity;
 
- 				showMeProduct();
+ 				// showMeProduct();
  				checkingOut(selection,amount);
-  });
+ 		})
 
 }//end of function showMeProduct
-
-	function checkingOut(idpro, qua) {
-		connection.query('SELECT * FROM Bamazon.Products WHERE idProducts = ' + idpro, function(err, result) {
+//------------------------------------------------------------------------------------
+	//Second function that let the user purchase the products.
+	function checkingOut(selection, amount) {
+		connection.query('SELECT * FROM Bamazon.Products WHERE idProducts = ' + selection, function(err, result) {
 		if (err) throw err;
-		console.log()
 
+			var productQ = selection -1;
+
+			 if(productQ > result.length || productQ <= 0 ){
+			 	console.log("Not enough merchandise");
+			 	
+			 return;
+			 } else {
+			 console.log('user may buy ' + result[0].ProductName)
+		}
+		
 	});
 };
+// 	function updateProduct(){
+// 		connection.query('UPDATE Bamazon.Products SET WHERE ? = ' + , function(err, result) {
+// 	});
+// };
 
  
 // connection.end();
